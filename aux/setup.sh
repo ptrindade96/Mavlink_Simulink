d2987439681ad3955638f836e17f0d04b6e01c24
Trindade <pedro210196@gmail.com>
2023-03-28 12:41:15 +0100

    Correct the setup script

--- aux/setup.sh
+++ aux/setup.sh
@@ -4,16 +4,16 @@
 if [ ! -f ~/.bashrc.bak ]; then
     cp ~/.bashrc ~/.bashrc.bak
     # Store ID numbers
-    echo -e '\n' >> "~/.bashrc"
-    echo -e 'id_user=$(id -u)\n' >> "~/.bashrc"
-    echo -e 'id_net=$((id_user-2000))\n' >> "~/.bashrc"
+    echo -e '\n' >> ".bashrc"
+    echo -e 'id_user=$(id -u)\n' >> ".bashrc"
+    echo -e 'id_net=$((id_user-2000))\n' >> ".bashrc"
     # Configure Gazebo/ROS ports
-    echo -e 'ros_port=$(($id_net+21400))\n' >> "~/.bashrc"
-    echo -e 'gazebo_port=$(($id_net+22500))\n' >> "~/.bashrc"
-    echo -e 'console_port=$(($id_net+23500))\n'  >> "~/.bashrc"
-    echo -e 'cpf_broadcast_port=$(($id_net+24500))\n' >> "~/.bashrc"
-    echo -e 'px4_start_port=$((25000+100*$id_net))\n' >> "~/.bashrc"
-    echo -e 'export ROS_MASTER_URI=http://localhost:$ros_port\n'  >> "~/.bashrc"
-    echo -e 'export GAZEBO_MASTER_URI=http://localhost:$gazebo_port'  >> "~/.bashrc"
-    echo -e 'alias 3dTiger="/opt/TurboVNC/bin/vncserver -kill :$id_net; /opt/TurboVNC/bin/vncserver -vgl -wm mate-session :$id_net"'  >> "~/.bashrc"
+    echo -e 'ros_port=$(($id_net+21400))\n' >> ".bashrc"
+    echo -e 'gazebo_port=$(($id_net+22500))\n' >> ".bashrc"
+    echo -e 'console_port=$(($id_net+23500))\n'  >> ".bashrc"
+    echo -e 'cpf_broadcast_port=$(($id_net+24500))\n' >> ".bashrc"
+    echo -e 'px4_start_port=$((25000+100*$id_net))\n' >> ".bashrc"
+    echo -e 'export ROS_MASTER_URI=http://localhost:$ros_port\n'  >> ".bashrc"
+    echo -e 'export GAZEBO_MASTER_URI=http://localhost:$gazebo_port'  >> ".bashrc"
+    echo -e 'alias 3dTiger="/opt/TurboVNC/bin/vncserver -kill :$id_net; /opt/TurboVNC/bin/vncserver -vgl -wm mate-session :$id_net"'  >> ".bashrc"
 fi
