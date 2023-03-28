eed189a690f90dae8396231d01cd1296f26e6469
Trindade <pedro210196@gmail.com>
2023-03-28 11:59:33 +0100

    Modify files to configure diferent IP addresses, depending on the user ID.

--- aux/gazebo-launch_dsor-multirotors.sh
+++ aux/gazebo-launch_dsor-multirotors.sh
@@ -35,7 +35,7 @@ function spawn_model() {
 	pushd "$working_dir" &>/dev/null
 	echo "starting instance $N in $(pwd)"
 	../bin/px4 -i $N -d "$build_path/etc" -w sitl_${MODEL}_${N} -s etc/init.d-posix/rcS >out.log 2>err.log &
-	python3 ${src_path}/Tools/sitl_gazebo/scripts/jinja_gen.py ${src_path}/Tools/sitl_gazebo/models/${MODEL}/${MODEL}.sdf.jinja ${src_path}/Tools/sitl_gazebo --mavlink_tcp_port $((4560+${N})) --mavlink_udp_port $((14540+${N})) --mavlink_id $((1+${N})) --gst_udp_port $((5600+${N})) --video_uri $((5600+${N})) --mavlink_cam_udp_port $((14530+${N})) --output-file /tmp/${MODEL}_${N}.sdf
+	python3 ${src_path}/Tools/sitl_gazebo/scripts/jinja_gen.py ${src_path}/Tools/sitl_gazebo/models/${MODEL}/${MODEL}.sdf.jinja ${src_path}/Tools/sitl_gazebo --mavlink_tcp_port $(($px4_start_port+${N})) --mavlink_udp_port $((14540+${N})) --mavlink_id $((1+${N})) --gst_udp_port $((5600+${N})) --video_uri $((5600+${N})) --mavlink_cam_udp_port $((14530+${N})) --output-file /tmp/${MODEL}_${N}.sdf
 
 	echo "Spawning ${MODEL}_${N} at ${X} ${Y}"
 
@@ -43,6 +43,45 @@ function spawn_model() {
 
 	popd &>/dev/null
 
+}
+
+function configure_simulation() {
+	px4_conf_file_dir=$1
+
+	sed -i 's/simulator_tcp_port=$((4560+px4_instance))/simulator_tcp_port=$(($px4_start_port+px4_instance))/g' ${px4_conf_file_dir}/px4-rc.simulator
+	sed -i 's/param set-default COM_RC_IN_MODE 1/param set-default COM_RC_IN_MODE 1\nparam set-default COM_RCL_EXCEPT 4/g' ${px4_conf_file_dir}/rcS
+	sed 's/udp_offboard_port_local=$((14580+px4_instance))/udp_offboard_port_local=$(($px4_start_port+20+px4_instance))/g' ${px4_conf_file_dir}/px4-rc.mavlink > ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/udp_onboard_payload_port_local=$((14280+px4_instance))/udp_onboard_payload_port_local=$(($px4_start_port+40+px4_instance))/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/udp_onboard_gimbal_port_local=$((13030+px4_instance))/udp_onboard_gimbal_port_local=$(($px4_start_port+60+px4_instance))/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/udp_gcs_port_local=$((18570+px4_instance))/udp_gcs_port_local=$(($px4_start_port+80+px4_instance))\ntarget_ip=TEMPLATE_IP/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/mavlink start -x -u $udp_gcs_port_local -r 4000000 -f/mavlink start -x -u $udp_gcs_port_local -r 4000000 -f -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/mavlink start -x -u $udp_offboard_port_local -r 4000000 -f -m onboard -o $udp_offboard_port_remote/mavlink start -x -u $udp_offboard_port_local -r 60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s POSITION_TARGET_LOCAL_NED -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 50 -s LOCAL_POSITION_NED -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s GLOBAL_POSITION_INT -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 50 -s ATTITUDE -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ATTITUDE_QUATERNION -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ATTITUDE_TARGET -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s SERVO_OUTPUT_RAW_0 -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s RC_CHANNELS -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s OPTICAL_FLOW_RAD -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ODOMETRY -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s HIGHRES_IMU -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 1 -s HEARTBEAT -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s VFR_HUD -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 2 -s TIMESYNC -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ALTITUDE -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/mavlink start -x -u $udp_onboard_payload_port_local -r 4000 -f -m onboard -o $udp_onboard_payload_port_remote/mavlink start -x -u $udp_onboard_payload_port_local -r 4000 -f -m onboard -o $udp_onboard_payload_port_remote -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	sed -i 's/mavlink start -x -u $udp_onboard_gimbal_port_local -r 400000 -m gimbal -o $udp_onboard_gimbal_port_remote/mavlink start -x -u $udp_onboard_gimbal_port_local -r 400000 -m gimbal -o $udp_onboard_gimbal_port_remote -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
+	if [ -f "${px4_conf_file_dir}/airframes/10016_iris" ]; then
+		echo -e '\nparam set-default MC_PITCHRATE_K 2\nparam set-default MC_ROLLRATE_K 2\nparam set-default MC_ROLL_P 8\nparam set-default MC_PITCH_P 8' >> "${px4_conf_file_dir}/airframes/10016_iris"
+		echo -e '\nparam set-default MC_ROLLRATE_MAX 1600\nparam set-default MC_PITCHRATE_MAX 1600\nparam set-default MC_YAWRATE_MAX 1000' >> "${px4_conf_file_dir}/airframes/10016_iris"
+	else
+		if [ -f "${px4_conf_file_dir}/airframes/10016_gazebo-classic_iris" ]; then
+			echo -e '\nparam set-default MC_PITCHRATE_K 2\nparam set-default MC_ROLLRATE_K 2\nparam set-default MC_ROLL_P 8\nparam set-default MC_PITCH_P 8' >> "${px4_conf_file_dir}/airframes/10016_gazebo-classic_iris"
+			echo -e '\nparam set-default MC_ROLLRATE_MAX 1600\nparam set-default MC_PITCHRATE_MAX 1600\nparam set-default MC_YAWRATE_MAX 1000' >> "${px4_conf_file_dir}/airframes/10016_gazebo-classic_iris"
+		fi
+	fi
 }
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]
@@ -78,42 +117,12 @@ src_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 build_path=${src_path}/build/${target}
 
 # Change IP of Mavlink connection
-px4_conf_file_dir=${src_path}/ROMFS/px4fmu_common/init.d-posix
-if [ ! -f "${px4_conf_file_dir}/px4-rc.mavlink.template" ]; then
+if [ ! -f "${src_path}/ROMFS/px4fmu_common/init.d-posix/px4-rc.mavlink.template" ]; then
   # If the file px4-rc.mavlink.template does not exist, it is the first run.
   # Then some configurations must be made
-  sed -i 's/param set-default COM_RC_IN_MODE 1/param set-default COM_RC_IN_MODE 1\nparam set-default COM_RCL_EXCEPT 4/g' ${px4_conf_file_dir}/rcS
-  sed 's/udp_gcs_port_local=$((18570+px4_instance))/udp_gcs_port_local=$((18570+px4_instance))\ntarget_ip=TEMPLATE_IP/g' ${px4_conf_file_dir}/px4-rc.mavlink > ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i 's/mavlink start -x -u $udp_gcs_port_local -r 4000000 -f/mavlink start -x -u $udp_gcs_port_local -r 4000000 -f -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i 's/mavlink start -x -u $udp_offboard_port_local -r 4000000 -f -m onboard -o $udp_offboard_port_remote/mavlink start -x -u $udp_offboard_port_local -r 60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s POSITION_TARGET_LOCAL_NED -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 50 -s LOCAL_POSITION_NED -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s GLOBAL_POSITION_INT -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 50 -s ATTITUDE -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ATTITUDE_QUATERNION -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ATTITUDE_TARGET -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s SERVO_OUTPUT_RAW_0 -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s RC_CHANNELS -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s OPTICAL_FLOW_RAD -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ODOMETRY -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s HIGHRES_IMU -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 1 -s HEARTBEAT -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s VFR_HUD -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 2 -s TIMESYNC -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i '/60000 -f -m onboard -o $udp_offboard_port_remote -t $target_ip/s/$/\nmavlink stream -r 0 -s ALTITUDE -u $udp_offboard_port_local/' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i 's/mavlink start -x -u $udp_onboard_payload_port_local -r 4000 -f -m onboard -o $udp_onboard_payload_port_remote/mavlink start -x -u $udp_onboard_payload_port_local -r 4000 -f -m onboard -o $udp_onboard_payload_port_remote -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  sed -i 's/mavlink start -x -u $udp_onboard_gimbal_port_local -r 400000 -m gimbal -o $udp_onboard_gimbal_port_remote/mavlink start -x -u $udp_onboard_gimbal_port_local -r 400000 -m gimbal -o $udp_onboard_gimbal_port_remote -t $target_ip/g' ${px4_conf_file_dir}/px4-rc.mavlink.template
-  if [ -f "${px4_conf_file_dir}/airframes/10016_iris" ]; then
-	  echo -e '\nparam set-default MC_PITCHRATE_K 2\nparam set-default MC_ROLLRATE_K 2\nparam set-default MC_ROLL_P 8\nparam set-default MC_PITCH_P 8' >> "${px4_conf_file_dir}/airframes/10016_iris"
-	  echo -e '\nparam set-default MC_ROLLRATE_MAX 1600\nparam set-default MC_PITCHRATE_MAX 1600\nparam set-default MC_YAWRATE_MAX 1000' >> "${px4_conf_file_dir}/airframes/10016_iris"
-  else
-	  if [ -f "${px4_conf_file_dir}/airframes/10016_gazebo-classic_iris" ]; then
-		  echo -e '\nparam set-default MC_PITCHRATE_K 2\nparam set-default MC_ROLLRATE_K 2\nparam set-default MC_ROLL_P 8\nparam set-default MC_PITCH_P 8' >> "${px4_conf_file_dir}/airframes/10016_gazebo-classic_iris"
-		  echo -e '\nparam set-default MC_ROLLRATE_MAX 1600\nparam set-default MC_PITCHRATE_MAX 1600\nparam set-default MC_YAWRATE_MAX 1000' >> "${px4_conf_file_dir}/airframes/10016_gazebo-classic_iris"
-	  fi
-  fi
+  configure_simulation "${src_path}/ROMFS/px4fmu_common/init.d-posix"
 fi
-sed "s/TEMPLATE_IP/${target_ip}/g" ${px4_conf_file_dir}/px4-rc.mavlink.template > ${px4_conf_file_dir}/px4-rc.mavlink
+sed "s/TEMPLATE_IP/${target_ip}/g" ${src_path}/ROMFS/px4fmu_common/init.d-posix/px4-rc.mavlink.template > ${src_path}/ROMFS/px4fmu_common/init.d-posix/px4-rc.mavlink
 echo $target_ip
 
 working_dir=$(pwd)
